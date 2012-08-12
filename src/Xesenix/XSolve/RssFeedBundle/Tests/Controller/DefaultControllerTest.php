0a66b9f687083cc0c8b27f52fda87e66ef78c9f3
Paweł Kapalla <pkapalla@xesenix.pl>
2012-08-12 13:28:43 +0200

    testy

--- src/Xesenix/XSolve/RssFeedBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Xesenix/XSolve/RssFeedBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,38 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
-    {
-        $client = static::createClient();
+	public function frazesProvider()
+	{
+		return array(
+			array('Cloud Computing'),
+			array('na oficjalnym blogu Symfony'),
+			array('aaa'),
+			array('xxx'),
+			array('później'),
+			array(''),
+		);
+	}
+	
+	/**
+	 * @dataProvider frazesProvider
+	 */
+	public function testIndex($fraze)
+	{
+		$client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
-    }
+		$crawler = $client->request('GET', '/?fraze=' . $fraze);
+		
+		if ($fraze !== '')
+		{
+			$containsFraze = $crawler->filter('html:contains("' . $fraze . '")')->count() > 0;
+			$didnotFoundAnything = $crawler->filter('html:contains("Nie znaleziono wpisów zawierających szukaną frazę")')->count() > 0;
+			
+			$this->assertTrue($containsFraze || $didnotFoundAnything);
+		}
+		else
+		{
+			$didFoundSomething = $crawler->filter('html:contains("Nie znaleziono wpisów zawierających szukaną frazę")')->count() === 0;
+			$this->assertTrue($didFoundSomething);
+		}
+	}
 }
